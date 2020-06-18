1d2d0119c5eba6f0d378477951179c55af639b3e
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-06-18 16:48:44 +0530

    Initialized express app Installed graphql & express-graphql

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    
+}));
+
+app.listen(5000, () => {
+    console.log('Listening on port 4000');
+});

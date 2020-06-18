a60eef42b486513b43f9e22cec65fbff45ed94bb
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-06-19 00:14:46 +0530

    Creating mongoose models

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/GraphQLNinja', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+mongoose.connection.once('open', () => {
+  console.log('Connected to the database');
+});
 
 app.use(
   '/graphql',

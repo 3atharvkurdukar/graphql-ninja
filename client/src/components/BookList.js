89b076af5b0e704f851dd2219407eab81b0ef35f
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-06-19 19:25:09 +0530

    Retrieving data & rendering it

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 class BookList extends Component {
+  displayBooks() {
+    let data = this.props.data;
+    if (data.loading) {
+      return <div>Loading books...</div>;
+    } else {
+      return data.books.map((book) => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  }
   render() {
     return (
       <div>
-        <ul id="book-list">
-          <li>Book Name</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);

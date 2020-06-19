fca95bbbd978bfe142e347e5ee008cc4ef4c008c
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-06-19 19:44:25 +0530

    External query files

---
+++ client/src/queries/queries.js
@@ -0,0 +1,19 @@
+import { gql } from 'apollo-boost';
+
+export const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`;
+
+export const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;

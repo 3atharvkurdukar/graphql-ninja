17d4d792545aece0dded67c4f41a61e2948f3437
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-06-19 14:22:00 +0530

    Added react client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="main">
+      <h1>Ninja's Reading List</h1>
+    </div>
+  );
+}
+
+export default App;

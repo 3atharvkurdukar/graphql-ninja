125e285f4cf4bb60df7a76ddadee2cd2c4395fcf
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-06-19 22:06:11 +0530

    Adding BookDetails component

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -17,3 +17,22 @@ export const getBooksQuery = gql`
     }
   }
 `;
+
+export const getBookQuery = gql`
+  query($id: ID!) {
+    book(id: $id) {
+      id
+      name
+      genre
+      author {
+        id
+        name
+        age
+        books {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
